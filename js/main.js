0fa6e373922cf4ade258d3ea22e30f5fce92eccd
IvanChencov973 <80408541+IvanChencov973@users.noreply.github.com>
2021-10-14 19:57:07 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function(){
+
+
+    $('.slider').slick({
+       arrows: false,
+       fade: true,
+       autoplay:3000,
+       dots: false
+    });
+
+    
+
+    $('.header-btn').on('click', function(){
+        $('.menu').addClass('active');
+    })
+
+    $('.close-btn').on('click', function(){
+        $('.menu').removeClass('active');
+    })
+
+
+});
